f3bcaf659b0a9ae5986003d9f7266df1a1586dbd
fernanda188 <fernandamendoza.1220@gmail.com>
2022-09-05 22:29:46 +0200

    variablefamilia

---
+++ src/main/java/com/mycompany/familia/Familia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.familia;
+
+/**
+ *
+ * @author fernanda
+ */
+public class Familia {
+
+    public static void main(String[] args) {
+        int familia = 5;
+             
+                
+        System.out.println("mi madre es: alicia mendoza");
+        System.out.println("su edad es 37");
+        System.out.println("mi papa es: jose gonzalez");
+        System.out.println("su edad es 38");
+        System.out.println("mi hermano es:geovany gonzalez");
+        System.out.println("su edad es 21");
+        System.out.println("mi abuela es carmen perez");
+        System.out.println("su edad es 65");
+        System.out.println("en total somos ");
+    }
+}
